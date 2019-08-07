bbd96d1a8f47dd69faf4ab0d2b56ded7d86791fc
AdamAhmads <53849000+AdamAhmads@users.noreply.github.com>
2019-08-07 13:26:13 +0800

    cubaan pertama

---
+++ Hobby/Hobby/Form2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Form1.Show()
+    End Sub
+End Class
---
+++ Hobby/Hobby/Form4.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Form1.Show()
+    End Sub
+End Class
